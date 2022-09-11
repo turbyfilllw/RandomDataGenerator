483b5a3b93a434498101e6c03dd96f8000c9a7bf
turbyfilllw <turbyfilllw@etsu.edu>
2022-09-11 04:53:51 -0400

    dependant

--- RandomDataGenerator/Dependant.cs
+++ RandomDataGenerator/Dependant.cs
@@ -8,9 +8,15 @@ namespace RandomDataGenerator
 {
     public class Dependant : Person
     {
+        private static Random random = new Random();
         public Dependant() 
         {
-            
+            start = DateTime.Now;
+            end = new DateTime(2032, 9, 10);
+            Range = (end - start).Days;
+            BirthDate = (start.AddDays(random.Next(Range)).AddHours(random.Next(0, 24)).AddMinutes(random.Next(0, 60)).AddSeconds(random.Next(0, 60)));
         }
+
+        
     }
 }
